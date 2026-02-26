05ba27badf7509b098331845b930149b764d1f3f
Jack Dickinson <jackdickinson090@hotmail.com>
2026-02-26 21:07:44 +0100

    update infoPopover and readme

--- components/InfoHoverCard.tsx
+++ components/InfoHoverCard.tsx
@@ -11,18 +11,23 @@ export function InfoHoverCard() {
       <HoverCardTrigger asChild>
         <Info className="text-neutral-700" size={30} />
       </HoverCardTrigger>
-      <HoverCardContent className="flex flex-col gap-0.5 bg-muted-foreground">
+      <HoverCardContent className="w-full flex flex-col gap-0.5 bg-muted-foreground">
         <h1>A visualisation of daylight length over time.</h1>
-        <div className="mb-2">
-          <ul className="list-disc pl-6">
-            <li>NextJS</li>
-            <li>ChartJS</li>
-            <li>ipgeolocation astronomy API</li>
-          </ul>
-        </div>
-        <a href="https://github.com/Jackarius090/daylight-site">
-          <h1>See GitHub repo here</h1>
+        <a
+          className="p-2 mb-4 rounded-md hover:bg-background-highlight"
+          href="https://github.com/Jackarius090/daylight-site"
+        >
+          https://github.com/Jackarius090/daylight-site
         </a>
+        <div>Made using:</div>
+        <ul className="list-disc pl-6 mb-2">
+          <li>NextJS</li>
+          <li>ChartJS</li>
+          <li>ipgeolocation astronomy API</li>
+          <li>Shadcn</li>
+          <li>Tailwindcss</li>
+          <li>date-fns</li>
+        </ul>
       </HoverCardContent>
     </HoverCard>
   );
