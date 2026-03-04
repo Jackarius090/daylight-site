9ef2282062571d501a736c025f939ff8af6ab20f
Jack Dickinson <jackdickinson090@hotmail.com>
2026-03-04 21:13:04 +0100

    reformat api route to get data once, then parse out days needed for diffrent timeunits

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,5 @@
 import { dataTypeDay } from "../../components/ShowData";
 import { add } from "date-fns/fp";
-import { differenceInDays } from "date-fns";
 import { type NextRequest } from "next/server";
 
 function convertHoursMinutesToMinutes(dayLengthInClockFormat: string) {
@@ -41,107 +40,54 @@ export async function GET(request: NextRequest) {
 
   console.log("fetching data...");
   try {
-    if (timeUnitQuery === "day") {
-      // get todays date and the date 1 month from now for the url query.
-      const todaysDateInIso = convertDateToCorrectAPIFormat(new Date());
-      const dateInOneMonth = add({ months: 1 }, new Date());
-      const dateInOneMonthInIso = convertDateToCorrectAPIFormat(dateInOneMonth);
-      // const differenceInDaysBetweenDates = differenceInDays(
-      //   dateInOneMonth,
-      //   new Date(),
-      // );
-      const url = `https://api.ipgeolocation.io/v2/astronomy/timeSeries?apiKey=${process.env.DAY_LENGTH_API_KEY}&dateStart=${todaysDateInIso}&dateEnd=${dateInOneMonthInIso}&location=${cityQuery}&elevation=10`;
-      data = await fetchData(url);
-      console.log("data fetched");
-    } else if (timeUnitQuery === "week") {
-      // instead of manking many api calls to gather data for 1 day every week for a year.
-      // This will gather time series data for 73 days at a time (the api limit is 90 days at one time and 365 devides nicely into 73 x 5), and then parse out the days needed.
+    // instead of manking many api calls to gather data for 1 day every week for a year.
+    // This will gather time series data for 73 days at a time (the api limit is 90 days at one time and 365 devides nicely into 73 x 5), and then parse out the days needed.
 
-      const urls = [];
-      let currentDate = new Date();
-      for (let i = 0; i < 5; i++) {
-        const datePlusInterval = add({ days: 73 }, currentDate);
-        const currentDateISO = convertDateToCorrectAPIFormat(currentDate);
-        const dateInNinetyDaysISO =
-          convertDateToCorrectAPIFormat(datePlusInterval);
-        urls.push(
-          fetchData(
-            `https://api.ipgeolocation.io/v2/astronomy/timeSeries?apiKey=${process.env.DAY_LENGTH_API_KEY}&dateStart=${currentDateISO}&dateEnd=${dateInNinetyDaysISO}&location=${cityQuery}&elevation=10`,
-          ),
-        );
-        currentDate = add({ days: 73 }, currentDate);
-      }
-``
-      data = await Promise.all(urls);
-      console.log("data fetched");
+    const urls = [];
+    let currentDate = new Date();
+    for (let i = 0; i < 5; i++) {
+      const datePlusInterval = add({ days: 73 }, currentDate);
+      const currentDateISO = convertDateToCorrectAPIFormat(currentDate);
+      const dateInNinetyDaysISO =
+        convertDateToCorrectAPIFormat(datePlusInterval);
+      urls.push(
+        fetchData(
+          `https://api.ipgeolocation.io/v2/astronomy/timeSeries?apiKey=${process.env.DAY_LENGTH_API_KEY}&dateStart=${currentDateISO}&dateEnd=${dateInNinetyDaysISO}&location=${cityQuery}&elevation=10`,
+        ),
+      );
+      currentDate = add({ days: 73 }, currentDate);
+    }
+    data = await Promise.all(urls);
+    console.log("data fetched");
 
-      // this takes the array of arrays that is received from the api call. It takes only the astronomy data and then flattens in the arrays into a single array.
-      data = {
-        astronomy: data
-          .map((threeMonthPeriod) => {
-            return threeMonthPeriod.astronomy;
-          })
-          .flat(1),
-      };
-      console.log(data);
+    // this takes the array of arrays that is received from the api call. It takes only the astronomy data and then flattens in the arrays into a single array.
+    data = {
+      astronomy: data
+        .map((threeMonthPeriod) => {
+          return threeMonthPeriod.astronomy;
+        })
+        .flat(1),
+    };
 
+    if (timeUnitQuery === "week") {
       data.astronomy = data.astronomy.filter((day, i) => {
         if ((i + 1) % 7 === 0) {
           return day;
         }
       });
-      console.log(data);
-
-      // const urls = [];
-      // let milliSeconds = Date.now();
-      // for (let i = 0; i < 52; i += 4) {
-      //   const date = new Date(milliSeconds).toISOString();
-      //   const formattedDate = date.slice(0, 10);
-      // urls.push(
-      //   fetchData(
-      //     `https://api.ipgeolocation.io/v2/astronomy/timeSeries?apiKey=${process.env.DAY_LENGTH_API_KEY}&dateStart=${formattedDate}&dateEnd=${formattedDate}&location=${cityQuery}&elevation=10`,
-      //   ),
-      // );
-      //   // add 1 week to milliseconds
-      //   const millisecondsInADay = 1000 * 60 * 60 * 24;
-      //   milliSeconds += millisecondsInADay * 7;
-      // }
-      // data = await Promise.all(urls);
-      // console.log("data fetched");
-      // data = {
-      //   astronomy: data.map((day) => {
-      //     return day.astronomy[0];
-      //   }),
-      // };
     } else if (timeUnitQuery === "month") {
-      const urls = [];
-      let milliSeconds = Date.now();
-      for (let i = 0; i < 12; i++) {
-        const date = new Date(milliSeconds).toISOString();
-        const formattedDate = date.slice(0, 10);
-        urls.push(
-          fetchData(
-            `https://api.ipgeolocation.io/v2/astronomy/timeSeries?apiKey=${process.env.DAY_LENGTH_API_KEY}&dateStart=${formattedDate}&dateEnd=${formattedDate}&location=${cityQuery}&elevation=10`,
-          ),
-        );
-        // add 1 month to milliseconds
-        const millisecondsInADay = 1000 * 60 * 60 * 24;
-        milliSeconds += millisecondsInADay * 30;
-      }
-      data = await Promise.all(urls);
-      console.log("data fetched");
-      data = {
-        astronomy: data.map((day) => {
-          return day.astronomy[0];
-        }),
-      };
+      data.astronomy = data.astronomy.filter((day, i) => {
+        if ((i + 1) % 30 === 0) {
+          return day;
+        }
+      });
     }
   } catch (error) {
     console.error("Failed to fetch data:", error);
     return Response.json({ error: "Failed to fetch data" }, { status: 500 });
   }
 
-  const reformattedData: dataTypeDay = data.astronomy.map(
+  const reformattedData: dataTypeDay[] = data.astronomy.map(
     (day: ApiDayResponse) => {
       return {
         date: day.date,
